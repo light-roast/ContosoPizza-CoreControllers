e021835301617c4114859b2e0871a3533d702b53
Daniel Echeverri <echeverri121@gmail.com>
2024-01-19 14:59:58 -0500

    Create getby id method in pizza controller

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -15,7 +15,16 @@ public class PizzaController : ControllerBase
     [HttpGet]
     public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
 
-    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if(pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
 
     // POST action
 
